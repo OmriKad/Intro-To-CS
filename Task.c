cf855fd798a48e10d7e0087eba226b4b16e9f487
Omri Kadmon <omrikad2@gmail.com>
2022-11-07 13:02:02 +0200

    Fix the data type conversions

--- Task.c
+++ Task.c
@@ -1,3 +1,4 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
 void main() {
@@ -12,28 +13,28 @@ void main() {
     printf("Welcome to MTA-Pizza!\n\n");
     printf("*****\n ***\n  *\n\n");
     printf("Please enter your ID number:\n");
-    scanf_s("%d", &id);
+    scanf("%d", &id);
     // Menu info print.
     printf("\nOur menu:\n");
     printf("*********\n");
-    printf("Basic pizza: 60.00 NIS for 40X40 size pizza\n");
+    printf("Basic pizza: 60.00 NIS for 40x40 size pizza\n");
     printf("Toppings for basic size pizza:\n");
     printf("Olives: 10 NIS\n");
     printf("Mushrooms: 12 NIS\n\n");
     // The customer chooses pizza size and stored accordingly. The total basic price (toppings not included) is calculated and adds up.  
     printf("Please enter your pizza's length (cm): ");
-    scanf_s("%d", &length);
+    scanf("%d", &length);
     printf("Please enter your pizza's width (cm): ");
-    scanf_s("%d", &width);
-    total = ((width * length) / 1600) * 60;
+    scanf("%d", &width);
+    total = ((width * length) / 1600.0) * 60.0;
     // Customer chooses optional toppings and the area they cover the pizza. Price is added accordingly to the selected area. 
     printf("\nPlease choose the toppings:\n");
     printf("\nOlives - Enter 1 for the whole pizza\n0.5 for half\n0.25 for quarter\nor 0 for none: ");
-    scanf_s("%f", &topping1);
-    total += ((width * length * topping1) / 1600) * 10;
+    scanf("%f", &topping1);
+    total += ((width * length * topping1) / 1600.0) * 10.0;
     printf("\nMushrooms - Enter 1 for the whole pizza\n0.5 for half\n0.25 for quarter\nor 0 for none: ");
-    scanf_s("%f", &topping2);
-    total += ((width * length * topping2) / 1600) * 12;
+    scanf("%f", &topping2);
+    total += ((width * length * topping2) / 1600.0) * 12.0;
     // Optional delivery.
     printf("\nDo you want delivery for the price of 15 NIS? Enter 1 for delivery or 0 for pick-up: ");
     scanf_s("%d", &delivery);
